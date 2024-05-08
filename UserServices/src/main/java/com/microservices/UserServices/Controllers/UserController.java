8e80d338a34523a79c4b985568929be105314f91
Salman siddiqui <salmansiddiqui80900@gmail.com>
2024-05-08 21:18:34 +0530

    Todays update

--- UserServices/src/main/java/com/microservices/UserServices/Controllers/UserController.java
+++ UserServices/src/main/java/com/microservices/UserServices/Controllers/UserController.java
@@ -2,6 +2,7 @@ package com.microservices.UserServices.Controllers;
 
 import com.microservices.UserServices.entities.User;
 import com.microservices.UserServices.services.UserServices;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ public class UserController
     @Autowired
     private UserServices userServices;
 
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user)
     {
@@ -24,10 +26,18 @@ public class UserController
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name="ratingHotelBreaker" , fallbackMethod = "ratingHotelFallBackMethod")
     public ResponseEntity<User> getUser(@PathVariable String userId)
     {
         User user=userServices.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //fallback method of ratingHotelBreaker
+    public ResponseEntity<User> ratingHotelFallBackMethod(String id,Exception ex)
+    {
+        User user = User.builder().email("dummy@gmail.com").name("dummy").about("This is dummy user").userId("1234").build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     @GetMapping
