ea127cabd553f2a531eb503f56a1dde491ffcfcc
Salman siddiqui <salmansid8090@gmail.com>
2025-03-11 05:59:33 +0530

    Update

--- RatingServices/src/main/java/com/rating/config/SecurityConfiguration.java
+++ RatingServices/src/main/java/com/rating/config/SecurityConfiguration.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration
 {
